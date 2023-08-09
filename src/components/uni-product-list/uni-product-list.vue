56b71c37dd0f2e844883c2685b871e6716423b69
cruldra <cruldra@gmail.com>
2023-08-09 19:28:58 +0800

    11

---
+++ src/components/uni-product-list/uni-product-list.vue
@@ -0,0 +1,162 @@
+<script setup lang="ts">
+  import { defineProps } from 'vue';
+
+  defineProps({
+    products: {
+      type: Array,
+      required: true,
+    },
+    coverImgField: {
+      type: String,
+      required: false,
+      default: 'img',
+    },
+  });
+</script>
+
+<template>
+  <view class="product-list">
+    <view class="product-item" v-for="(item, index) in products" :key="index">
+      <view class="product-img">
+        <img alt="Random image from unsplash" :src="item[coverImgField]" class="css-gui-fjakwv" />
+      </view>
+      <view class="product-metas">
+        <view class="product-name">{{ item.name }}</view>
+        <view class="product-desc">{{ item.desc }}</view>
+        <view class="product-actions">
+          <text class="product-price">{{ item.price }}</text>
+          <view class="zixun-btn">
+            <up-button
+              color="linear-gradient(to right, rgb(66, 83, 216), rgb(213, 51, 186))"
+              type="primary"
+              size="mini"
+              text="咨询"
+            ></up-button>
+          </view>
+
+          <!--          <button class="mini-btn" type="warn" size="mini">按钮</button>-->
+        </view>
+      </view>
+    </view>
+  </view>
+</template>
+
+<style scoped lang="scss">
+  .product-list {
+    //-webkit-align-items: center;
+    //-webkit-box-align: center;
+    //-ms-flex-align: center;
+    //align-items: center;
+    gap: 19px;
+    -webkit-flex-direction: column;
+    -ms-flex-direction: column;
+    flex-direction: column;
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex;
+    //height: 1062px;
+    //width: 576px;
+    //background-color: #768e7d;
+    //margin: 80px;
+    padding: 0px;
+  }
+
+  .product-list .product-item:active {
+    transform: scale(0.95);
+  }
+  .product-list .product-item {
+    transition: transform 0.2s;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+    //background-color: rgba(188, 160, 160, 0.43);
+    -webkit-box-pack: center;
+    -ms-flex-pack: center;
+    -webkit-justify-content: center;
+    justify-content: center;
+    gap: 8px;
+    //height: 131px;
+    //width: 467px;
+    //background-color: #a26d63;
+    padding: 6px;
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex;
+  }
+  .product-list .product-item .product-img {
+    width: 35%;
+    //height: 100%;
+    //background-color: #9e67a2;
+  }
+  .css-gui-fjakwv {
+    width: 100%;
+    height: 100%;
+    border-radius: 15px;
+  }
+  .product-list .product-item .product-metas {
+    gap: 5px;
+    width: 65%;
+    //height: 100%;
+    //background-color: #877370;
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-flex-direction: column;
+    -ms-flex-direction: column;
+    flex-direction: column;
+  }
+  .product-list .product-item .product-metas .product-name {
+    //height: 24px;
+    color: #2e2e2e;
+    font-weight: bold;
+    font-size: 13px;
+    word-break: break-all;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 2;
+  }
+  .product-list .product-item .product-metas .product-desc {
+    color: #a0a0a0;
+    max-width: 100%;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    margin-top: 1.067vw;
+    font-size: 12px;
+    line-height: 5.067vw;
+  }
+  .product-list .product-item .product-metas .product-actions {
+    -webkit-align-items: flex-end;
+    -webkit-box-align: flex-end;
+    -ms-flex-align: flex-end;
+    align-items: flex-end;
+    -webkit-box-pack: justify;
+    -webkit-justify-content: space-between;
+    justify-content: space-between;
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex;
+    //background-color: #3331ab;
+    -webkit-flex: 1 1 0px;
+    -ms-flex: 1 1 0px;
+    flex: 1;
+  }
+  .css-gui-wxx2q4 {
+    height: 88px;
+    width: 462px;
+    background-color: #644540;
+  }
+
+  .product-list .product-item .product-metas .product-actions .product-price {
+    color: red;
+    font-weight: bold;
+  }
+  .zixun-btn {
+    //width: 20px;
+    margin-right: 0;
+  }
+</style>
---
+++ src/pages/index/search.vue
@@ -0,0 +1,135 @@
+<script setup lang="ts">
+  const searchValue = ref('');
+</script>
+
+<template>
+  <view class="page">
+    <u-search class="cars-search-bar page-item" placeholder="日照香炉生紫烟" v-model="searchValue"></u-search>
+
+    <!--    <uni-section class="page-item" title="基础用法"></uni-section>-->
+
+    <div data-v-2066a382="" class="hots">
+      <div data-v-2066a382="" class="hots-header"><span data-v-2066a382="" class="hots-title">热门搜索</span></div>
+      <div data-v-2066a382="" class="hots-main">
+        <div data-v-2066a382="" class="hots-selector">
+          <div
+            data-v-2066a382=""
+            class="icon"
+            style="background-image: url('http://img.souche.com/files/brand/0mqGUDiWbj.png')"
+          ></div>
+          <div data-v-2066a382="" class="name">奔驰</div>
+        </div>
+        <div data-v-2066a382="" class="hots-selector">
+          <div
+            data-v-2066a382=""
+            class="icon"
+            style="background-image: url('http://img.souche.com/files/brand/ik403ZDSAX.png')"
+          ></div>
+          <div data-v-2066a382="" class="name">宝马</div>
+        </div>
+        <div data-v-2066a382="" class="hots-selector">
+          <div
+            data-v-2066a382=""
+            class="icon"
+            style="background-image: url('http://img.souche.com/files/brand/WG6tTxwZKi.png')"
+          ></div>
+          <div data-v-2066a382="" class="name">丰田</div>
+        </div>
+        <div data-v-2066a382="" class="hots-selector">
+          <div
+            data-v-2066a382=""
+            class="icon"
+            style="background-image: url('http://img.souche.com/files/brand/bBAheBn2RL.png')"
+          ></div>
+          <div data-v-2066a382="" class="name">大众</div>
+        </div>
+        <div data-v-2066a382="" class="hots-selector">
+          <div
+            data-v-2066a382=""
+            class="icon"
+            style="background-image: url('http://img.souche.com/files/brand/ZSaCy7dE8c.png')"
+          ></div>
+          <div data-v-2066a382="" class="name">比亚迪</div>
+        </div>
+        <div data-v-2066a382="" class="hots-selector">
+          <div
+            data-v-2066a382=""
+            class="icon"
+            style="background-image: url('http://img.souche.com/files/brand/KG441Un6Km.png')"
+          ></div>
+          <div data-v-2066a382="" class="name">奥迪</div>
+        </div>
+        <div data-v-2066a382="" class="hots-selector">
+          <div
+            data-v-2066a382=""
+            class="icon"
+            style="background-image: url('http://img.souche.com/files/brand/jpQ5rosd4Z.png')"
+          ></div>
+          <div data-v-2066a382="" class="name">本田</div>
+        </div>
+        <div data-v-2066a382="" class="hots-selector">
+          <div
+            data-v-2066a382=""
+            class="icon"
+            style="background-image: url('http://img.souche.com/files/brand/lh4kDQf7rw.png')"
+          ></div>
+          <div data-v-2066a382="" class="name">马自达</div>
+        </div>
+        <div data-v-2066a382="" class="hots-selector">
+          <div
+            data-v-2066a382=""
+            class="icon"
+            style="background-image: url('http://img.souche.com/files/brand/fRgPVAtVkH.png')"
+          ></div>
+          <div data-v-2066a382="" class="name">英菲尼迪</div>
+        </div>
+        <div data-v-2066a382="" class="hots-selector">
+          <div
+            data-v-2066a382=""
+            class="icon"
+            style="background-image: url('http://img.souche.com/files/brand/fK6PQzLAgz.png')"
+          ></div>
+          <div data-v-2066a382="" class="name">吉利汽车</div>
+        </div>
+      </div>
+    </div>
+  </view>
+</template>
+
+<style scoped lang="scss">
+  .hots {
+    line-height: 1;
+  }
+  .hots .hots-header {
+    padding: 0 5.333vw;
+    font-size: 3.733vw;
+    font-weight: 700;
+    color: #131212;
+  }
+  .hots .hots-main {
+    display: flex;
+    flex-wrap: wrap;
+    padding: 2.4vw 3.6vw;
+  }
+  .hots .hots-main .hots-selector {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-right: 0.8vw;
+    width: 15.5vw;
+    height: 15.5vw;
+    font-size: 3.2vw;
+    color: #5e5e66;
+  }
+  .hots .hots-main .hots-selector .icon {
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: 50%;
+    height: 8.533vw;
+    width: 8.533vw;
+  }
+  .hots .hots-main .hots-selector .name {
+    margin-top: 2.133vw;
+  }
+</style>
